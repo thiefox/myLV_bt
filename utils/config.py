a954884ebc5a655d89ae6f12ae1270c7e789110d
thiefox <thiefox@gmail.com>
2024-06-20 12:54:05 +0800

    add sub dirs

---
+++ utils/config.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Module: config
+
+"""
+
+import json
+
+
+class Config:
+
+    def __init__(self):
+        self.market: str = "spot"  # 交易的平台
+        self.streams: list = None  # 交易steams
+        self.api_key: str = None
+        self.api_key_type: str = None
+        self.secret_path: str = None
+        self.api_secret: str = None
+        self.private_key: str = None
+        self.private_key_pass: str = None
+        self.pass_phrase = None
+        self.max_orders = 1
+        self.proxy_host = ""  # proxy host
+        self.proxy_port = 0  # proxy port
+        self.dingding_token: str = None  #dingding access_token
+        self.dingding_prompt = ""  #dingding key_word
+    #载入配置文件
+    def loads(self, config_file=None):
+        """ Load config file.
+
+        Args:
+            config_file: config json file.
+        """
+        configures = {}
+        if config_file:
+            try:
+                with open(config_file) as f:
+                    data = f.read()
+                    configures = json.loads(data)
+            except Exception as e:
+                print(e)
+                exit(0)
+            if not configures:
+                print("config json file error!")
+                exit(0)
+        self._update(configures)
+
+    def _update(self, update_fields : dict):
+        """
+        更新update fields.
+        :param update_fields:
+        :return: None
+        """
+        for k, v in update_fields.items():
+            setattr(self, k, v)
+
+        try:
+            if self.api_key_type == "HMAC":
+                with open(self.secret_path, 'r') as f:
+                    self.api_secret = f.read().strip('\n')
+            else:
+                with open(self.secret_path, 'rb') as f:
+                    self.private_key =  f.read() 
+        except Exception as e:
+            print(e)
+            exit(0)
+
+#config实例
+config = Config()
---
+++ utils/utility.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Module: utils
+"""
+
+import requests
+import json
+from pathlib import Path
+from decimal import Decimal
+
+def _get_trader_dir(temp_name: str):
+    """
+    Get path where trader is running in.
+    """
+    cwd = Path.cwd()
+    temp_path = cwd.joinpath(temp_name)
+
+    if temp_path.exists():
+        return cwd, temp_path
+
+    if not temp_path.exists():
+        temp_path.mkdir()
+
+    return cwd, temp_path
+
+
+TRADER_DIR, TEMP_DIR = _get_trader_dir("trader")
+
+
+def get_file_path(filename: str):
+    """
+    Get path for temp file with filename.
+    """
+    return TEMP_DIR.joinpath(filename)
+
+
+def get_folder_path(folder_name: str):
+    """
+    Get path for temp folder with folder name.
+    """
+    folder_path = TEMP_DIR.joinpath(folder_name)
+    if not folder_path.exists():
+        folder_path.mkdir()
+    return folder_path
+
+def load_json(filename: str):
+    """
+    Load data from json file in temp path.
+    """
+    filepath = get_file_path(filename)
+
+    if filepath.exists():
+        with open(filepath, mode="r", encoding="UTF-8") as f:
+            data = json.load(f)
+        return data
+    else:
+        save_json(filename, {})
+        return {}
+
+
+def save_json(filename: str, data: dict):
+    """
+    Save data into json file in temp path.
+    """
+    filepath = get_file_path(filename)
+    with open(filepath, mode="w+", encoding="UTF-8") as f:
+        json.dump(
+            data,
+            f,
+            indent=4,
+            ensure_ascii=False
+        )
+
+
+def round_to(value: float, target: float) -> float:
+    """
+    Round price to price tick value.
+    """
+    value = Decimal(str(value))
+    target = Decimal(str(target))
+    rounded = float(int(round(value / target)) * target)
+    return rounded
+
+def dingding_info(token: str, prompt: str, symbol: str, content: str):
+    headers = {'Content-Type': 'application/json;charset=utf-8'}
+    api_url = "https://oapi.dingtalk.com/robot/send?access_token=%s" % token
+    info_text = "%s %s %s" % (prompt, symbol, content)
+    json_text = {
+        "msgtype": "text",
+        "at": {
+            "atMobiles": [],
+            "isAtAll": False
+        },
+        "text": {
+            "content": info_text
+        }
+    }
+    requests.post(api_url, json.dumps(json_text), headers=headers).content
